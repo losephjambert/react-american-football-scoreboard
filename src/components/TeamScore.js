13881504c12c6c834f66ada045b9df909c71d177
Joe <josephc.lambert@gmail.com>
2019-09-09 14:15:30 -0700

    stubs TeamScore component

---
+++ src/components/TeamScore.js
@@ -0,0 +1,15 @@
+//TODO: STEP 1 - Import the useState hook.
+import React from 'react';
+import './App.css';
+
+const TeamScore = (teamName, score) => {
+  return (
+    <div className='team'>
+      <h2 className='team__name'>{teamName}</h2>
+
+      <div className='team__score'>{score}</div>
+    </div>
+  );
+};
+
+export default TeamScore;
