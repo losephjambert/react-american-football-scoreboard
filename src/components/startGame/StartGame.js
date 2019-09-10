db0d700739f5447f1fd4bc0ceb867a1b15225996
Joe <josephc.lambert@gmail.com>
2019-09-10 08:32:30 -0700

    styles form and changes period length and numPeriods to fieldsets in the form

--- src/components/startGame/StartGame.js
+++ src/components/startGame/StartGame.js
@@ -29,93 +29,172 @@ export default function StartGame() {
   return (
     <div className='form-container'>
       <form action='' onSubmit={e => handleSubmit(e)}>
-        <div>
-          <label htmlFor='homeTeamName'>Home Team Name:</label>
-          <input id='homeTeamName' type='text' onChange={e => handleOnChange('homeTeamName', e.target.value)} />
+        <div className='home-team form-section'>
+          <h3>Home Team</h3>
+          <div>
+            <label htmlFor='homeTeamName'>Home Team Name</label>
+            <input id='homeTeamName' type='text' onChange={e => handleOnChange('homeTeamName', e.target.value)} />
+          </div>
+          <div>
+            <fieldset>
+              <legend>Home Team Color</legend>
+              <div>
+                <input
+                  id='homeTeamRed'
+                  type='radio'
+                  name='homeColor'
+                  value='red'
+                  onChange={e => handleOnChange('homeTeamColor', e.target.value)}
+                />
+                <label htmlFor='homeTeamRed'>Red</label>
+              </div>
+              <div>
+                <input
+                  id='homeTeamBlue'
+                  type='radio'
+                  name='homeColor'
+                  value='lightblue'
+                  onChange={e => handleOnChange('homeTeamColor', e.target.value)}
+                />
+                <label htmlFor='homeTeamBlue'>Blue</label>
+              </div>
+              <div>
+                <input
+                  id='homeTeamSalmon'
+                  type='radio'
+                  name='homeColor'
+                  value='salmon'
+                  onChange={e => handleOnChange('homeTeamColor', e.target.value)}
+                />
+                <label htmlFor='homeTeamSalmon'>Salmon</label>
+              </div>
+            </fieldset>
+          </div>
         </div>
-        <div>
-          <fieldset>
-            <legend>Home Team Color:</legend>
-            <label htmlFor='homeTeamRed'>Red</label>
-            <input
-              id='homeTeamRed'
-              type='radio'
-              name='color'
-              value='red'
-              onChange={e => handleOnChange('homeTeamColor', e.target.value)}
-            />
-            <label htmlFor='homeTeamBlue'>Blue</label>
-            <input
-              id='homeTeamBlue'
-              type='radio'
-              name='color'
-              value='lightblue'
-              onChange={e => handleOnChange('homeTeamColor', e.target.value)}
-            />
-            <label htmlFor='homeTeamSalmon'>Salmon</label>
-            <input
-              id='homeTeamSalmon'
-              type='radio'
-              name='color'
-              value='salmon'
-              onChange={e => handleOnChange('homeTeamColor', e.target.value)}
-            />
-          </fieldset>
+        <div className='away-team form-section'>
+          <h3>Away Team</h3>
+          <div>
+            <label htmlFor='awayTeamName'>Away Team Name</label>
+            <input id='awayTeamName' type='text' onChange={e => handleOnChange('awayTeamName', e.target.value)} />
+          </div>
+          <div>
+            <fieldset>
+              <legend>Away Team Color</legend>
+              <div>
+                <input
+                  id='awayTeamOrange'
+                  type='radio'
+                  name='awayColor'
+                  value='orange'
+                  onChange={e => handleOnChange('awayTeamColor', e.target.value)}
+                />
+                <label htmlFor='awayTeamOrange'>Orange</label>
+              </div>
+              <div>
+                <input
+                  id='awayTeamGoldenrod'
+                  type='radio'
+                  name='awayColor'
+                  value='goldenrod'
+                  onChange={e => handleOnChange('awayTeamColor', e.target.value)}
+                />
+                <label htmlFor='awayTeamGoldenrod'>Goldenrod</label>
+              </div>
+              <div>
+                <input
+                  id='awayTeamPurple'
+                  type='radio'
+                  name='awayColor'
+                  value='rebeccapurple'
+                  onChange={e => handleOnChange('awayTeamColor', e.target.value)}
+                />
+                <label htmlFor='awayTeamPurple'>Purple</label>
+              </div>
+            </fieldset>
+          </div>
         </div>
-        <div>
-          <label htmlFor='awayTeamName'>Away Team Name:</label>
-          <input id='awayTeamName' type='text' onChange={e => handleOnChange('awayTeamName', e.target.value)} />
-        </div>
-        <div>
-          <fieldset>
-            <legend>Away Team Color:</legend>
-            <label htmlFor='awayTeamOrange'>Orange</label>
-            <input
-              id='awayTeamOrange'
-              type='radio'
-              name='color'
-              value='orange'
-              onChange={e => handleOnChange('awayTeamColor', e.target.value)}
-            />
-            <label htmlFor='awayTeamGoldenrod'>Goldenrod</label>
-            <input
-              id='awayTeamGoldenrod'
-              type='radio'
-              name='color'
-              value='goldenrod'
-              onChange={e => handleOnChange('awayTeamColor', e.target.value)}
-            />
-            <label htmlFor='awayTeamPurple'>Purple</label>
-            <input
-              id='awayTeamPurple'
-              type='radio'
-              name='color'
-              value='rebeccapurple'
-              onChange={e => handleOnChange('awayTeamColor', e.target.value)}
-            />
-          </fieldset>
-        </div>
-        <div>
-          <label htmlFor='periodLength'>Period Length:</label>
-          <input
-            id='periodLength'
-            type='number'
-            min={1}
-            max={12}
-            placeholder={12}
-            onChange={e => handleOnChange('periodLength', e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor='numPeriods'>Number of Periods:</label>
-          <input
-            id='numPeriods'
-            type='number'
-            min={1}
-            max={4}
-            placeholder={1}
-            onChange={e => handleOnChange('numPeriods', e.target.value)}
-          />
+        <div className='game-details form-section'>
+          <h3>Game Details</h3>
+          <div>
+            <fieldset>
+              <legend>Period Length</legend>
+              <div>
+                <input
+                  id='fourMinutes'
+                  type='radio'
+                  name='periodLength'
+                  value='4'
+                  onChange={e => handleOnChange('periodLength', Number(e.target.value))}
+                />
+                <label htmlFor='fourMinutes'>4 minutes</label>
+              </div>
+              <div>
+                <input
+                  id='eightMinutes'
+                  type='radio'
+                  name='periodLength'
+                  value='8'
+                  onChange={e => handleOnChange('periodLength', Number(e.target.value))}
+                />
+                <label htmlFor='eightMinutes'>8 minutes</label>
+              </div>
+              <div>
+                <input
+                  id='twelveMinutes'
+                  type='radio'
+                  name='periodLength'
+                  value='12'
+                  onChange={e => handleOnChange('periodLength', Number(e.target.value))}
+                />
+                <label htmlFor='twelveMinutes'>12 minutes</label>
+              </div>
+            </fieldset>
+          </div>
+          <div>
+            <fieldset>
+              <legend>Number of Periods</legend>
+              <div>
+                <input
+                  id='oneP'
+                  type='radio'
+                  name='numPeriods'
+                  value='1'
+                  onChange={e => handleOnChange('numPeriods', Number(e.target.value))}
+                />
+                <label htmlFor='oneP'>1 period</label>
+              </div>
+              <div>
+                <input
+                  id='twoP'
+                  type='radio'
+                  name='numPeriods'
+                  value='2'
+                  onChange={e => handleOnChange('numPeriods', Number(e.target.value))}
+                />
+                <label htmlFor='twoP'>2 periods</label>
+              </div>
+              <div>
+                <input
+                  id='threeP'
+                  type='radio'
+                  name='numPeriods'
+                  value='3'
+                  onChange={e => handleOnChange('numPeriods', Number(e.target.value))}
+                />
+                <label htmlFor='threeP'>3 periods</label>
+              </div>
+              <div>
+                <input
+                  id='fourP'
+                  type='radio'
+                  name='numPeriods'
+                  value='4'
+                  onChange={e => handleOnChange('numPeriods', Number(e.target.value))}
+                />
+                <label htmlFor='fourP'>4 periods</label>
+              </div>
+            </fieldset>
+          </div>
         </div>
         <div>
           <button type='submit'>Start Game</button>
